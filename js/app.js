1129295e7bca7b1d8bbaf9fdec24b9df8b914a39
Samuel SIlva <samuelsilvawb@gmail.com>
2018-02-13 10:04:40 -0300

    style : remove functions from global scope
    - created IIFE to remove functions from global scope
    - created constant to endpoint

--- js/app.js
+++ js/app.js
@@ -1,60 +1,60 @@
-window.onload = afterLoad;
-
-function afterLoad() {
-    getForecastTopCapitalBrazil();
-};
-
-/**
- * @param city: string 
- * Must contain city, state
- * e.g 'Fortaleza, CE'
- */
-function _searchCapital(city) {
-
-    const apiToSearch = `https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city}')&u=c&format=json`;
-    fetch(apiToSearch)
-        .then(_toJson)
-        .then(console.log)
-        .catch(console.log);
-};
-
-function getForecastTopCapitalBrazil() {
-
-    const capitals = [
-        'Rio de Janeiro',
-        'São Paulo',
-        'Belo Horizonte',
-        'Brasilia',
-        'Belém',
-        'Salvador',
-        'Curitiba',
-        'Fortaleza',
-        'Manaus',
-        'João Pessoa'
-    ];
+window.onload = (function () {
     
-    const endPoint = function(capital) {
-        return window.encodeURI(`https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${capital}' )&u=c&format=json`);
-    };
+    const END_POINT = 'https://query.yahooapis.com/v1/public/yql';
+    getForecastTopCapitalBrazil();
+    
+    /**
+     * @param city: string 
+     * Must contain city, state
+     * e.g 'Fortaleza, CE'
+     */
+    function _searchCapital(city) {
 
-    capitals.forEach((capital) => {
-        fetch(endPoint(capital))
+        const apiToSearch = `${END_POINT}?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city}')&u=c&format=json`;
+        fetch(apiToSearch)
             .then(_toJson)
             .then(console.log)
             .catch(console.log);
-    });
-}
+    };
 
-function _toJson(data) {
-    return data.json().then(json => json);
-}
+    function getForecastTopCapitalBrazil() {
 
-/*--  SEARCH HANDLER --*/
+        const capitals = [
+            'Rio de Janeiro',
+            'São Paulo',
+            'Belo Horizonte',
+            'Brasilia',
+            'Belém',
+            'Salvador',
+            'Curitiba',
+            'Fortaleza',
+            'Manaus',
+            'João Pessoa'
+        ];
 
-document.querySelector('.form').addEventListener('submit', handleSubmitSearch);
+        const createURL = function (capital) {
+            return window.encodeURI(`${END_POINT}?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${capital}' )&u=c&format=json`);
+        };
 
-function handleSubmitSearch(e) {
-    e.preventDefault();
-    const valueInputSearch = document.querySelector('.form__search-city').value;
-    _searchCapital(valueInputSearch);
-}
+        capitals.forEach((capital) => {
+            fetch(createURL(capital))
+                .then(_toJson)
+                .then(console.log)
+                .catch(console.log);
+        });
+    }
+
+    function _toJson(data) {
+        return data.json().then(json => json);
+    }
+
+    /*--  SEARCH HANDLER --*/
+
+    document.querySelector('.form').addEventListener('submit', handleSubmitSearch);
+
+    function handleSubmitSearch(e) {
+        e.preventDefault();
+        const valueInputSearch = document.querySelector('.form__search-city').value;
+        _searchCapital(valueInputSearch);
+    }
+})();
