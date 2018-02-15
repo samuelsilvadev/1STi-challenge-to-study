40e15b194bdee6b248e9d6cace6ba7db557475f8
Samuel SIlva <samuelsilvawb@gmail.com>
2018-02-15 17:17:49 -0300

    feat : create HTML of the capitals temperature
    - created function to convert fahrenheit in celsius
    - created functions to insert HTML in view

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 window.onload = (function () {
-    
+
     const END_POINT = 'https://query.yahooapis.com/v1/public/yql';
     getForecastTopCapitalBrazil();
-    
+
     /**
      * @param city: string 
      * Must contain city, state
@@ -18,6 +18,7 @@ window.onload = (function () {
     };
 
     function getForecastTopCapitalBrazil() {
+        let count = 0;
 
         const capitals = [
             'Rio de Janeiro',
@@ -33,19 +34,47 @@ window.onload = (function () {
         ];
 
         const createURL = function (capital) {
-            return window.encodeURI(`${END_POINT}?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${capital}' )&u=c&format=json`);
+            return window.encodeURI(`${END_POINT}?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${capital}')&u=c&format=json`);
+        };
+
+        const createTr = function (data) {
+            count++;
+            if (data.query.results) {
+                const baseObj = data.query.results.channel;
+                const location = baseObj.location.city;
+                const item = baseObj.item.forecast[0];
+                return `<tr>
+                        <td>${fahrenheitToCelsius(item.low)}°</td>
+                        <td>${fahrenheitToCelsius(item.high)}°</td>
+                        <td>${location}</td>
+                    </tr>`;
+            }
+            throw new Error('Invalid Data');
+        };
+
+        const addTrInView = function (tr) {            
+            const idToPutTr = count <= 5 ? '#tbodyLeft' : '#tbodyRight';
+            document.querySelector(idToPutTr).innerHTML += tr;            
         };
 
         capitals.forEach((capital) => {
             fetch(createURL(capital))
                 .then(_toJson)
-                .then(console.log)
+                .then(createTr)                
+                .then(addTrInView)                
                 .catch(console.log);
         });
     }
 
     function _toJson(data) {
-        return data.json().then(json => json);
+        return data.json().then(json => { 
+            console.log(json);
+            return json;
+        });
+    }
+
+    function fahrenheitToCelsius(temperature) {
+        return Math.floor((temperature - 32) * (5/9));
     }
 
     /*--  SEARCH HANDLER --*/
