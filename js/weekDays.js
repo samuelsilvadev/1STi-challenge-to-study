9a65f086623fe77309b0741920df006f95608668
Samuel SIlva <samuelsilvawb@gmail.com>
2018-02-16 15:18:14 -0300

    feat : create constant to hadle with days
    - created object that treat days

---
+++ js/weekDays.js
@@ -0,0 +1,14 @@
+/** 
+ * constant to handle with week days came from weather API
+ */
+const week = {
+    Sun: 'Domingo',
+    Mon: 'Segunda',
+    Tue: 'Terça',
+    Wed: 'Quarta',
+    Thu: 'Quinta',
+    Fri: 'Sexta',
+    Sat: 'Sábado'
+};
+
+module.exports = week;
