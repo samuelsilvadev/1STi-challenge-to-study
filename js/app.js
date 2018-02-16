22cba5d8cbcb6f0ab2c77716b4e35f1d97b185c5
Samuel SIlva <samuelsilvawb@gmail.com>
2018-02-16 15:19:46 -0300

    feat : create card component
    - created component card when search something
    - created helper file
    - updated the ids and classes to handle with elements

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+'use strict'
+
 window.onload = (function () {
+    
+    const Utils = require('./helpers');
+    const conditionsCode = require('./conditionsCodes');
+    const week = require('./weekDays');
 
     const END_POINT = 'https://query.yahooapis.com/v1/public/yql';
+    const $ = Utils.$;
+
     getForecastTopCapitalBrazil();
 
     /**
@@ -11,9 +19,50 @@ window.onload = (function () {
     function _searchCapital(city) {
 
         const apiToSearch = `${END_POINT}?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city}')&u=c&format=json`;
+
+        const createCard = function (data) {
+
+            if (data.query.results) {
+
+                const baseObj = data.query.results.channel;
+                const location = baseObj.location;
+                const item = baseObj.item.forecast[0];
+                const wind = baseObj.wind;
+                const humidity = baseObj.atmosphere.humidity;
+
+                $('.card__time__title').innerText = `${location.city}, ${location.region} - ${location.country}`;
+                $('.card__time__forecast').innerText = `${Utils.fahrenheitToCelsius(item.high)}°C ${conditionsCode[item.code]}`;
+                $('#lowTime').innerText = `${Utils.fahrenheitToCelsius(item.low)}°`
+                $('#highTime').innerText = `${Utils.fahrenheitToCelsius(item.high)}°`
+                $('#humidity').innerText = `${humidity}%`
+                $('#sensation').innerText = `${Utils.fahrenheitToCelsius(wind.chill)}°`
+                $('#wind').innerText = `${wind.speed} Km/h`
+                $('.card__time').classList.remove('display--none');
+
+                createWeekDays(baseObj.item.forecast);
+            }
+        };
+
+        const createWeekDays = function (forecast) {
+            let htmlCardTimeDays = '';
+            if (forecast && forecast.length > 0 && Array.isArray(forecast)) {
+                forecast = forecast.slice(1, 6);
+                htmlCardTimeDays = forecast.map(day => {
+                    return ` <div>
+                                <h4>${week[day.day]}</h4>
+                                <div class="color--dark-orange">
+                                    <span>${Utils.fahrenheitToCelsius(day.low)}º</span>
+                                    <span>${Utils.fahrenheitToCelsius(day.high)}º</span>
+                                </div>
+                            </div>`
+                }).join('');
+                $('.card__time__days').innerHTML = htmlCardTimeDays;
+            }
+        };
+
         fetch(apiToSearch)
-            .then(_toJson)
-            .then(console.log)
+            .then(Utils.toJson)
+            .then(createCard)
             .catch(console.log);
     };
 
@@ -44,37 +93,26 @@ window.onload = (function () {
                 const location = baseObj.location.city;
                 const item = baseObj.item.forecast[0];
                 return `<tr>
-                        <td>${fahrenheitToCelsius(item.low)}°</td>
-                        <td>${fahrenheitToCelsius(item.high)}°</td>
+                        <td>${Utils.fahrenheitToCelsius(item.low)}°</td>
+                        <td>${Utils.fahrenheitToCelsius(item.high)}°</td>
                         <td>${location}</td>
                     </tr>`;
             }
             throw new Error('Invalid Data');
         };
 
-        const addTrInView = function (tr) {            
+        const addTrInView = function (tr) {
             const idToPutTr = count <= 5 ? '#tbodyLeft' : '#tbodyRight';
-            document.querySelector(idToPutTr).innerHTML += tr;            
+            document.querySelector(idToPutTr).innerHTML += tr;
         };
 
         capitals.forEach((capital) => {
             fetch(createURL(capital))
-                .then(_toJson)
-                .then(createTr)                
-                .then(addTrInView)                
+                .then(Utils.toJson)
+                .then(createTr)
+                .then(addTrInView)
                 .catch(console.log);
         });
-    }
-
-    function _toJson(data) {
-        return data.json().then(json => { 
-            console.log(json);
-            return json;
-        });
-    }
-
-    function fahrenheitToCelsius(temperature) {
-        return Math.floor((temperature - 32) * (5/9));
     }
 
     /*--  SEARCH HANDLER --*/
---
+++ js/helpers.js
@@ -0,0 +1,22 @@
+/**
+ * Function used to facilite get elements from view
+ * @param {selector} element 
+ */
+function $(element) {
+    return document.querySelector(element);
+};
+
+function toJson(data) {
+    return data.json().then(json => {
+        console.log(json);
+        return json;
+    });
+};
+
+function fahrenheitToCelsius(temperature) {
+    return Math.floor((temperature - 32) * (5 / 9));
+};
+
+exports.$ = $;
+exports.toJson = toJson;
+exports.fahrenheitToCelsius = fahrenheitToCelsius;
